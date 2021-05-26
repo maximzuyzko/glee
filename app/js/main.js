55d632959de4890df866a16a2ae724405aebbd48
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-05-26 11:17:14 +0400

    fix mixer

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,32 @@ $(function () {
     $('.product-slider__items').slick({
         dots: true,
         arrows: false,
-        /*autoplay: true,
-        autoplaySpeed: 5000,*/
+        autoplay: true,
+        autoplaySpeed: 5000,
     })
 
-    var mixer = mixitup('.products-week__items');
-})
+    /*var mixer = mixitup('.products-week__items');*/
+
+    /*var mixerOne = mixitup('.products-week__items', {
+        controls: {
+            scope: 'local'
+        }
+    });
+
+    var mixerTwo = mixitup('.design__items', {
+        controls: {
+            scope: 'local'
+        }
+    });*/
+
+    let containerEl1 = document.querySelector('.products-week');
+    let containerEl2 = document.querySelector('.design');
+    let config = {
+        controls: {
+            scope: 'local',
+        },
+    };
+    (() => mixitup(containerEl1, config))();
+    (() => mixitup(containerEl2, config))();
+
+});
