654134292256bb99c9ab255783113e65bcf59703
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-05-30 18:18:38 +0400

    added media

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ $(function () {
     $('.product-slider__items').slick({
         dots: true,
         arrows: false,
+        centerPadding: 0,
         /*autoplay: true,
         autoplaySpeed: 5000,*/
     })
@@ -12,6 +13,36 @@ $(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 350,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     let containerEl1 = document.querySelector('.products-week');
@@ -29,4 +60,17 @@ $(function () {
         $('.menu--header').toggleClass('menu--active');
         $('.body').toggleClass('body-stop-scroll');
     });
+
+    $(window).scroll(function () {
+        var fixed = $('.header__inner');
+        var slider = $('.product-slider')
+        scroll = $(window).scrollTop();
+        if (scroll >= 70) {
+            fixed.addClass('header__inner--scroll')
+            slider.addClass('product-slider--scroll')
+        } else {
+            fixed.removeClass('header__inner--scroll')
+            slider.removeClass('product-slider--scroll')
+        }
+    })
 });
