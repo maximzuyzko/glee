93c2ef656f41b3eb9e26fd92c294e2afd2a12fe2
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-06-06 14:10:04 +0400

    added product list, fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,7 @@ function scripts() {
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.min.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
